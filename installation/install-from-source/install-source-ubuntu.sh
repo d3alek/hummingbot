cbdfcde9a4ae584d1c77718d408e07bdeaa93a7c
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2022-03-12 07:23:11 +0200

    Fix install script

--- installation/install-from-source/install-source-ubuntu.sh
+++ installation/install-from-source/install-source-ubuntu.sh
@@ -1,3 +1,5 @@
+HOME=/home/$USER
+cd $HOME
 # SETUP DEPENDENCIES
 # 1) Install dependencies
 sudo apt-get update
@@ -5,13 +7,9 @@ sudo apt-get install -y build-essential
 # 2) Install Miniconda3
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
-export CONDAPATH="$(pwd)/miniconda3"
-export PYTHON="$(pwd)/miniconda3/envs/hummingbot/bin/python3"
-# INSTALL HUMMINGBOT
-# 3) Clone Hummingbot
-git clone https://github.com/hummingbot/hummingbot.git
-# 4) Install Hummingbot
-export hummingbotPath="$(pwd)/hummingbot" && cd $hummingbotPath && ./install
+export CONDAPATH="$HOME/miniconda3"
+export PYTHON="$HOME/miniconda3/envs/hummingbot/bin/python3"
+export hummingbotPath="$HOME/hummingbot" && cd $hummingbotPath && git checkout funding_rate && ./install
 # 5) Activate environment and compile code
 ${CONDAPATH}/bin/activate hummingbot && ${PYTHON} setup.py build_ext --inplace
 # 6) Start Hummingbot
